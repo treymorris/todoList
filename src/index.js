234c761951955fec73472734986e1b588f08bfe2
Trey Morris <treymorrisit@hotmail.com>
2021-11-01 16:44:32 -0400

    got a basic layout

--- src/index.js
+++ src/index.js
@@ -1,4 +1,78 @@
 import './style.css';
 
+(function() {
 
-alert('hello');
+    const taskmaster = {
+        projects : [],
+        init: function() {
+            this.cacheDom();
+        },
+        cacheDom: function() {
+            this.list = document.querySelector('ul');
+            this.myNodeList = document.getElementsByTagName("LI");
+            this.close = document.getElementsByClassName("close");
+            this.add = document.getElementById("add");
+        },
+        render: function() {
+            
+        }
+    };
+
+    taskmaster.init();
+
+})();
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+const add = document.getElementById("add");
+add.addEventListener('click', newElement)
+
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("todotext").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+      alert("You must write something!");
+    } else {
+      document.getElementById("list").appendChild(li);
+    }
+    document.getElementById("todotext").value = "";
+  
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+  
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
+  }
